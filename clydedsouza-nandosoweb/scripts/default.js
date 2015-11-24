926bfa06b02c130d2debcd1e259c21b9001dff1e
Clyde D'Souza <clyd94@gmail.com>
2015-11-24 14:36:35 +1300

    Commit while resolving the other api issue

---
+++ clydedsouza-nandosoweb/scripts/default.js
@@ -0,0 +1,30 @@
+﻿headerText = "<div class=' navbar navbar-inverse navbar-fixed-top nav-header'>";
+headerText += "<div class='container'>";
+headerText += "<div class='navbar-header'>";
+headerText += "<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>";
+headerText += "<span class='icon-bar'></span>";
+headerText += "<span class='icon-bar'></span>";
+headerText += "<span class='icon-bar'></span>";
+headerText += "</button>";
+headerText += "<a class='navbar-brand' href='/'>Nandoso</a>";
+headerText += "</div>";
+headerText += "<div class='navbar-collapse collapse navbar-right'>";
+headerText += "<ul class='nav navbar-nav'>";
+headerText += "<li><a href='/'>Home</a></li>";
+headerText += "<li><a href='/menu'>Menu</a></li>";
+headerText += "<li><a href='/specials'>Specials</a></li>";
+headerText += "<li><a href='/feedback'>Feedback</a></li>";
+headerText += "<li><a href='/find-us'>Find Us</a></li>";
+headerText += "</ul>";
+headerText += "</div>";
+headerText += "</div>";
+headerText += "</div>";
+headerText += "<div class='container-fluid custom-jumbotron-container'>";
+headerText += "<div class='jumbotron text-center'>";
+headerText += "<h1>Nandoso</h1>";
+headerText += "</div>";
+headerText += "</div>";
+
+function loadHeader() {
+
+}
